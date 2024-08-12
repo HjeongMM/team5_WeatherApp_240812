240a340eea5bb7ec6d5e8352740a23417bb7a245
HjeongMM <mmhj0129@naver.com>
2024-08-12 21:15:12 +0900

    초기 세팅 완료
    기타 라이브러리는 필요해지면.

---
+++ WetherApp/Controller/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  WetherApp
+//
+//  Created by 임혜정 on 8/12/24.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
---
+++ WetherApp/View/MainView.swift
@@ -0,0 +1,9 @@
+//
+//  MainView.swift
+//  WetherApp
+//
+//  Created by 임혜정 on 8/12/24.
+//
+
+import UIKit
+import SnapKit
